9f4ddfd7302cb7adc72614a0617b0999829dec58
Ankit Singh <142376440+chauhanAni5959@users.noreply.github.com>
2025-10-29 09:47:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+const textInput = document.getElementById('text');
+const delayInput = document.getElementById('delay');
+const btn = document.getElementById('btn');
+const outputDiv = document.getElementById('output');
+
+
+function wait(ms) {
+	return new Promise(resolve=> setTimeout(resolve, ms));
+	
+}
+
+btn.addEventListener('click', async function (event){
+	event.preventDefault();
+	outputDiv.textContent = '';
+	const message = textInput.value;
+	const delay = Number(delayInput.value);
+
+	if(!message){
+		outputDiv.textContent = 'Please enter a message.';
+		return;
+		
+	}
+	if(!delay || delay < 0){
+		outputDiv.textContent = 'Please enter a valid non-negative delay';
+		return;
+	}
+
+	await wait(delay);
+
+	outputDiv.textContent = message;
+
+
+	
+});
